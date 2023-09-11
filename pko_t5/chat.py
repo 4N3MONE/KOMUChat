1423c42a28e283b2db8e79f3f7c3583c8d616a3c
minalang <73943090+minalang@users.noreply.github.com>
2023-09-11 17:20:27 +0900

    Update chat.py

--- pko_t5/chat.py
+++ pko_t5/chat.py
@@ -6,11 +6,12 @@ from tqdm import tqdm
 
 def model_init(args):
         # vanilla model일 경우 arg대신 아래와 같이 모델이름을 넣어주면 됨
-        model_name = 'paust/pko-t5-large'
-        #model = T5ForConditionalGeneration.from_pretrained(args.model_path)
-        model = T5ForConditionalGeneration.from_pretrained(model_name)                
-        #tokenizer = T5TokenizerFast.from_pretrained(args.model_path)
-        tokenizer = T5TokenizerFast.from_pretrained(model_name)
+        # model_name = 'paust/pko-t5-base' #'paust/pko-t5-large'
+        model = T5ForConditionalGeneration.from_pretrained(args.model_path)
+        # model = T5ForConditionalGeneration.from_pretrained(model_name)                
+        tokenizer = T5TokenizerFast.from_pretrained(args.model_path)
+        # tokenizer = T5TokenizerFast.from_pretrained(model_name)
+        
         # add tokens
         tokenizer.add_tokens(["#화자#", "#청자#", "#(남자)청자#", "#(남자)화자#", "#(여자)청자#", "(여자)화자"])
         model.resize_token_embeddings(len(tokenizer))
@@ -51,7 +52,7 @@ def generate_from_file(model, tokenizer, args):
         
 if __name__ == '__main__':
         parser = argparse.ArgumentParser(description='Hyperparameter for Chat')
-        parser.add_argument('--model_path', default='/home/work/deeptext/COMU-ChatBot/model/pko_t5_wel_patience10', type=str,
+        parser.add_argument('--model_path', default='./model', type=str,
                                                 help='model path for chat') 
         parser.add_argument('--max_input_length', default=64, type=int,
                                                 help='user max input length')
@@ -59,13 +60,15 @@ if __name__ == '__main__':
                                                 help='max target length')
         parser.add_argument('--prefix', default='qa question: ', type=str,
                                                 help='inference input prefix')
-        parser.add_argument('--qpath', default='/home/work/deeptext/COMU-ChatBot/data/qlist.csv', type=str,
+        parser.add_argument('--qpath', default='./qlist.csv', type=str,
                                                 help='question list')
         parser.add_argument('--outputpath', default='./output.csv', type=str,
                                                 help='output file save')
         args = parser.parse_args()
 
         model, tokenizer = model_init(args)
-        generate_from_file(model, tokenizer, args)
-        print('done.')
-        # sequantially_chat(model, tokenizer, args)
+        sequantially_chat(model, tokenizer, args)
+        
+        # if you want to get a answer list from question file
+        # generate_from_file(model, tokenizer, args)
+        # print('done.')
